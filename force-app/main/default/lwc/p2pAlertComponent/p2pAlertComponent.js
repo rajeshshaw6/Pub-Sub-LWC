3ec6eb45fc49d69180519520335f6b668c9f8c5d
Rajesh Shaw <43552295+rajeshshaw6@users.noreply.github.com>
2022-12-30 00:22:32 +0530

    Delete force-app/main/default/lwc/p2pAlertComponent directory

--- force-app/main/default/lwc/p2pAlertComponent/p2pAlertComponent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement,api } from 'lwc';
-
-export default class P2pAlertComponent extends LightningElement {
-    @api message;
-    @api cardHeading;
-    @api number;
-    @api isValid;
-}
